0d7f743ba4ed15515f6bb564d04d8f50210fd5f5
buuraak77 <brkylmz8@hotmail.com>
2022-08-09 16:05:58 +0300

    Initial Commit

---
+++ Table View With Button Trying/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table View With Button Trying
+//
+//  Created by Burak Yılmaz on 9.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
